944bd9d0ed432be63d709f0624551bfd8966cc61
affanrivandi <75370721+affanrivandi@users.noreply.github.com>
2020-12-13 23:26:43 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,6 +1,5 @@
 var developmentDatabase = {
     postgres: {
-<<<<<<< HEAD
         host: 'ec2-54-246-115-40.eu-west-1.compute.amazonaws.com',
         port: 5432,
         database: 'dflff9f30e6905',
@@ -10,17 +9,6 @@ var developmentDatabase = {
         }
         
         var connectionString = "postgres://cnboidckfdikgk:c220709e9e6c556da6745085934c4c482d01e2ded8aac9a47f5e76cd36cd4d24@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dflff9f30e6905";
-=======
-    host: 'ec2-54-246-87-132.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    database: 'dhlf4nkng1qpa',
-    user: 'idrdkfxwtqqoea',
-    password: '6defdfc5f89e72c1d6a54cbf8f778354fbb1a958866646e9eff43709372b320e'
-    }
-    }
-    
-    var connectionString = "postgres://idrdkfxwtqqoea:6defdfc5f89e72c1d6a54cbf8f778354fbb1a958866646e9eff43709372b320e@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/dhlf4nkng1qpa";
->>>>>>> 4277ea94043d6c6b93dcb106aba4b250096c5cc6
     if (process.env.NODE_ENV == 'production') {
     //Production mode
     if (process.env.DATABASE_URL) {
